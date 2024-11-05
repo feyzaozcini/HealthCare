3efa5fdb7c27bb64b6303dc3c19339cbbc14370d
nil1lik <birliknil@gmail.com>
2024-11-05 14:45:17 +0300

    Dataaccess implementations added

---
+++ src/Pusula.Training.HealthCare.EntityFrameworkCore/Countries/EfCoreCountryRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pusula.Training.HealthCare.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace Pusula.Training.HealthCare.Countries;
+
+public class EfCoreCountryRepository(IDbContextProvider<HealthCareDbContext> dbContextProvider)
+    : EfCoreRepository<HealthCareDbContext, Country, Guid>(dbContextProvider), ICountryRepository
+{
+    public virtual async Task DeleteAllAsync(
+        string? filterText = null, 
+        string? name = null, 
+        string? code = null, 
+        CancellationToken cancellationToken = default)
+    {
+        var query = await GetQueryableAsync();
+
+        query = ApplyFilter(query, filterText, name, code);
+
+        var ids = query.Select(x => x.Id);
+        await DeleteManyAsync(ids, cancellationToken: GetCancellationToken(cancellationToken));
+    }
+
+    public virtual async Task<long> GetCountAsync(
+        string? filterText = null, 
+        string? name = null, 
+        string? code = null, 
+        CancellationToken cancellationToken = default)
+    {
+        var query = ApplyFilter((await GetDbSetAsync()), filterText, name, code);
+        return await query.LongCountAsync(GetCancellationToken(cancellationToken));
+    }
+
+    public async Task<List<Country>> GetListAsync(
+        string? filterText = null, 
+        string? name = null, 
+        string? code = null, 
+        string? sorting = null, 
+        int maxResultCount = int.MaxValue, 
+        int skipCount = 0, 
+        CancellationToken cancellationToken = default
+        )
+    {
+        var query = ApplyFilter((await GetQueryableAsync()), filterText, name, code);
+        query = query.OrderBy(string.IsNullOrWhiteSpace(sorting) ? CountryConsts.GetDefaultSorting(false) : sorting);
+        return await query.PageBy(skipCount, maxResultCount).ToListAsync(cancellationToken);
+    }
+
+
+    protected virtual IQueryable<Country> ApplyFilter(
+        IQueryable<Country> query,
+        string? filterText = null,
+        string? name = null,
+        string? code = null)
+    {
+        return query
+                .WhereIf(!string.IsNullOrWhiteSpace(filterText), e => e.Name!.Contains(filterText!) || e.Code!.Contains(filterText!))
+                .WhereIf(!string.IsNullOrWhiteSpace(name), e => e.Name.Contains(name!))
+                .WhereIf(!string.IsNullOrWhiteSpace(code), e => e.Code.Contains(code!));
+    }
+}
---
+++ src/Pusula.Training.HealthCare.EntityFrameworkCore/Migrations/20241104123106_CountryTableAdded.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pusula.Training.HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class CountryTableAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppCountries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    Code = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uuid", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppCountries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppCountries");
+        }
+    }
+}
