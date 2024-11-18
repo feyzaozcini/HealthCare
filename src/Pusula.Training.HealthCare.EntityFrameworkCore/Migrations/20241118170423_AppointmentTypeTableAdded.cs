6b257b3117dbfce8df0064953f5c05a8d20a3918
feyzaozcini <feyza_ozcini@hotmail.com>
2024-11-18 21:01:00 +0300

    Appointment Type Services Permisson and Migration Added

---
+++ src/Pusula.Training.HealthCare.EntityFrameworkCore/Migrations/20241118170423_AppointmentTypeTableAdded.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pusula.Training.HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppointmentTypeTableAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppAppointmentTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uuid", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppAppointmentTypes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppAppointmentTypes");
+        }
+    }
+}
