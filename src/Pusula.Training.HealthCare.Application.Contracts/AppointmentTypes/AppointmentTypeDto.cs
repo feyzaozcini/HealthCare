94a0ecf5af133a628c0a14e97233ef4f987ed0ce
feyzaozcini <feyza_ozcini@hotmail.com>
2024-11-18 16:20:58 +0300

    Appointment Type Service Added

---
+++ src/Pusula.Training.HealthCare.Application.Contracts/AppointmentTypes/AppointmentTypeDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Pusula.Training.HealthCare.AppointmentTypes
+{
+    public class AppointmentTypeDto : AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ src/Pusula.Training.HealthCare.Application.Contracts/AppointmentTypes/GetAppointmentTypesInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace Pusula.Training.HealthCare.AppointmentTypes
+{
+    public class GetAppointmentTypesInput : PagedAndSortedResultRequestDto
+    {
+        public string? FilterText { get; set; }
+
+        public string? Name { get; set; }
+
+        public GetAppointmentTypesInput()
+        {
+        }
+    }
+}
