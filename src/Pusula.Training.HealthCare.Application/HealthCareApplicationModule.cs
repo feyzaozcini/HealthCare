0ebea32974502d2f2c19e86cf70c04561a660052
feyzaozcini <feyza_ozcini@hotmail.com>
2024-11-21 13:05:27 +0300

    Patient Card Added for Appointment Page. Appointment Table Added DB.

--- src/Pusula.Training.HealthCare.Application/HealthCareApplicationModule.cs
+++ src/Pusula.Training.HealthCare.Application/HealthCareApplicationModule.cs
@@ -91,6 +91,5 @@ public class HealthCareApplicationModule : AbpModule
         
         context.Services.AddTransient<UserProfileManager>();
         
-
     }
 }
---
+++ src/Pusula.Training.HealthCare.EntityFrameworkCore/Migrations/20241120142938_Appointment_Service_Added.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pusula.Training.HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class Appointment_Service_Added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppAppointments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Note = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    AppointmentStatus = table.Column<int>(type: "integer", nullable: false),
+                    PatientId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DoctorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DepartmentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AppointmentTypeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ExtraProperties = table.Column<string>(type: "text", nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uuid", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uuid", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    DeleterId = table.Column<Guid>(type: "uuid", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppAppointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppAppointments_AppAppointmentTypes_AppointmentTypeId",
+                        column: x => x.AppointmentTypeId,
+                        principalTable: "AppAppointmentTypes",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_AppAppointments_AppDepartments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "AppDepartments",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_AppAppointments_AppDoctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "AppDoctors",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_AppAppointments_AppPatients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "AppPatients",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppAppointments_AppointmentTypeId",
+                table: "AppAppointments",
+                column: "AppointmentTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppAppointments_DepartmentId",
+                table: "AppAppointments",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppAppointments_DoctorId",
+                table: "AppAppointments",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppAppointments_PatientId",
+                table: "AppAppointments",
+                column: "PatientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppAppointments");
+        }
+    }
+}
